2b2dc80df1dfccb565992bd14da0229871df2576
simooker <kyle@janrian.com>
2012-07-06 13:59:48 -0700

    javascript fx for duplicate uuid

--- javascripts/wall.js
+++ javascripts/wall.js
@@ -1,30 +1,45 @@
 jQuery(document).ready(function($) {	
+	var screenHeight; var screenWidth; var facesHeight; var facesWidth; var msg1; var msg2; var speed;
+
+	msg1 = 'Just Registered'; // New Profile
+	msg2 = 'Welcome Back';		// Existing Profile
+	speed = 5000;							// Animation Speed
+	
 	PUBNUB.subscribe({
   	channel:"facepalm",
     restore:false,
     callback:function (message) {
     	console.log(message);			
-			$("#faces").append(
-				$(".face:first").clone().html(function(){
-					$(this).addClass(message.uuid);
-					if(message.photo_url){
-						$(this).find('.avatar').append('<span class="picture"><img src="' + message.photo_url + '"/></span>');
-					} else {
-						$(this).find('.avatar').append('<span class="janrain-icon-64 janrain-icon-person"></span>');
-					};
-					if(message.name){
-						$(this).find('.profile').append('<span class="name">'+message.name+'</span>');
-					};
-				}).removeClass('hide').fadeIn('slow')
-			);						
-			facewall();
+    	if($('#faces li').hasClass(message.uuid)){
+    		$('#faces li.'+message.uuid).find('.mini-message').text(msg2).fadeIn().delay(speed).fadeOut();
+    		// Pulse Glow
+    		/*setInterval(function(){
+    			$('#faces li.'+message.uuid).toggleClass('highlight');
+    		}, 1000);*/
+    		$('#faces li.'+message.uuid).addClass('highlight');
+    		setTimeout(function(){
+    			$('#faces li.'+message.uuid).removeClass('highlight');
+    		},speed);
+    	} else {
+				$("#faces").append(
+					$(".face:first").clone().html(function(){
+						$(this).addClass(message.uuid);
+						if(message.photo_url){
+							$(this).find('.avatar').append('<span class="picture"><img src="' + message.photo_url + '"/></span>');
+						} else {
+							$(this).find('.avatar').append('<span class="janrain-icon-64 janrain-icon-person"></span>');
+						};
+						if(message.name){
+							$(this).find('.profile').append('<span class="name">'+message.name+'</span>');
+						};
+						$(this).find('.mini-message').text(msg1).delay(speed).fadeOut();
+					}).removeClass('hide').fadeIn('slow')
+				);						
+				facewall();
+			}
     }
   });
-	
-	screenSize();
-	
-	// Set the global vars
-	var screenHeight; var screenWidth; var facesHeight; var facesWidth;
+
 	
 	// Gets the current dimensions of the window
 	function screenSize(){
